e948b159f6a250e6c32081dbdd11d480720458a5
Michal Szyszka <michalszyszka.m@gmail.com>
2024-11-04 20:05:25 +0100

    Initial commit

---
+++ app/src/main/java/szysz3/planty/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package szysz3.planty.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
