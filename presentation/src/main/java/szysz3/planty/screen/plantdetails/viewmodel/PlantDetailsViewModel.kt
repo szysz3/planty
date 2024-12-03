453b558a4716a63831bf4ae02a1a23e7280f75c9
Michal Szyszka <michalszyszka.m@gmail.com>
2024-12-03 16:30:29 +0100

    Remove unused class

--- presentation/src/main/java/szysz3/planty/screen/plantdetails/viewmodel/PlantDetailsViewModel.kt
+++
@@ -1,7 +0,0 @@
-package szysz3.planty.screen.plantdetails.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class PlantDetailsViewModel : ViewModel() {
-    // TODO: consider extracxting some logic from PlantAPlantViewModel and HomScreenViewModel
-}
