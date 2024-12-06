7efc0e9c932fe62c71cae0cade37d5f5c77b3d8d
Michal Szyszka <michalszyszka.m@gmail.com>
2024-12-06 13:45:55 +0100

    Add FAB to my garden screen

--- presentation/src/main/java/szysz3/planty/screen/mygarden/composable/EmptyGardenPlaceholder.kt
+++ presentation/src/main/java/szysz3/planty/screen/mygarden/composable/EmptyGardenPlaceholder.kt
@@ -1,27 +1,34 @@
 package szysz3.planty.screen.mygarden.composable
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import szysz3.planty.R
-import szysz3.planty.ui.widgets.RoundedButton
 
 @Composable
 fun EmptyGardenPlaceholder(onCreateNewMap: () -> Unit) {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Image(
-            painter = painterResource(id = R.drawable.placeholder),
-            contentDescription = "Garden map placeholder",
-            modifier = Modifier.size(200.dp)
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        RoundedButton(onClick = onCreateNewMap, text = "Create New Map")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.BottomEnd // For the Floating Action Button
+    ) {
+        FloatingActionButton(
+            onClick = { onCreateNewMap() },
+            modifier = Modifier
+                .padding(16.dp),
+            elevation = FloatingActionButtonDefaults.elevation(8.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Rounded.Add,
+                contentDescription = "Add garden"
+            )
+        }
     }
 }
