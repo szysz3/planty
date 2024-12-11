312c7d77430a80c8534576d2459292fa3c4f5e1a
Michal Szyszka <michalszyszka.m@gmail.com>
2024-12-11 11:27:06 +0100

    Update mappings and strings for plant details

--- presentation/src/main/java/szysz3/planty/screen/plantdetails/model/Mapper.kt
+++ presentation/src/main/java/szysz3/planty/screen/plantdetails/model/Mapper.kt
@@ -10,6 +10,8 @@ fun mapMoistureToString(moisture: String): String {
         "M" -> stringResource(R.string.moisture_medium)
         "DM" -> stringResource(R.string.moisture_dry_to_medium)
         "MWe" -> stringResource(R.string.moisture_medium_with_wetness)
+        "DMWe" -> stringResource(R.string.moisture_dry_to_medium_to_wet)
+        "We" -> stringResource(R.string.moisture_wet)
         else -> stringResource(R.string.moisture_unknown)
     }
 }
@@ -40,33 +42,36 @@ fun mapSoilToString(soil: String): String {
 @Composable
 fun mapShadeToString(shade: String): String {
     return when (shade) {
-        "SN" -> stringResource(R.string.shade_sun_partial) // Some Shade (Sun-Partial)
-        "FS" -> stringResource(R.string.shade_full_shade)  // Full Shade
-        "FSN" -> stringResource(R.string.shade_full_to_partial) // Full Shade to No Shade
-        "N" -> stringResource(R.string.shade_full_sun)  // Full Sun
-        else -> stringResource(R.string.shade_unknown)  // Unknown shade level
+        "SN" -> stringResource(R.string.shade_sun_partial)
+        "FS" -> stringResource(R.string.shade_full_shade)
+        "FSN" -> stringResource(R.string.shade_full_to_partial)
+        "S" -> stringResource(R.string.shade_partial)
+        "N" -> stringResource(R.string.shade_full_sun)
+        else -> stringResource(R.string.shade_unknown)
     }
 }
 
 @Composable
-fun mapFrostTenderToString(isFrostTender: Boolean): String {
-    return if (isFrostTender) {
-        stringResource(R.string.frost_tender_yes)
-    } else {
-        stringResource(R.string.frost_tender_no)
+fun mapFrostTenderToString(isFrostTender: Boolean?): String {
+    return when (isFrostTender) {
+        true -> stringResource(R.string.frost_tender_yes)
+        false -> stringResource(R.string.frost_tender_no)
+        null -> stringResource(R.string.frost_tender_unknown)
     }
 }
 
 @Composable
 fun mapHardinessToString(hardiness: Int): String {
     return when (hardiness) {
-        3 -> stringResource(R.string.hardiness_zone_3) // -40°C to -34°C
-        4 -> stringResource(R.string.hardiness_zone_4) // -34°C to -29°C
-        5 -> stringResource(R.string.hardiness_zone_5) // -29°C to -23°C
-        6 -> stringResource(R.string.hardiness_zone_6) // -23°C to -18°C
-        7 -> stringResource(R.string.hardiness_zone_7) // -18°C to -12°C
-        8 -> stringResource(R.string.hardiness_zone_8) // -12°C to -7°C
-        10 -> stringResource(R.string.hardiness_zone_10) // -1°C to +4°C
+        2 -> stringResource(R.string.hardiness_zone_2)
+        3 -> stringResource(R.string.hardiness_zone_3)
+        4 -> stringResource(R.string.hardiness_zone_4)
+        5 -> stringResource(R.string.hardiness_zone_5)
+        6 -> stringResource(R.string.hardiness_zone_6)
+        7 -> stringResource(R.string.hardiness_zone_7)
+        8 -> stringResource(R.string.hardiness_zone_8)
+        9 -> stringResource(R.string.hardiness_zone_9)
+        10 -> stringResource(R.string.hardiness_zone_10)
         else -> stringResource(R.string.hardiness_unknown)
     }
 }
@@ -74,45 +79,45 @@ fun mapHardinessToString(hardiness: Int): String {
 @Composable
 fun mapGrowthRateToString(growthRate: String): String {
     return when (growthRate) {
-        "S" -> stringResource(R.string.growth_rate_slow)   // Slow Growth Rate
-        "M" -> stringResource(R.string.growth_rate_medium) // Medium Growth Rate
-        "F" -> stringResource(R.string.growth_rate_fast)  // Fast Growth Rate
-        else -> stringResource(R.string.growth_rate_unknown) // Unknown Growth Rate
+        "S" -> stringResource(R.string.growth_rate_slow)
+        "M" -> stringResource(R.string.growth_rate_medium)
+        "F" -> stringResource(R.string.growth_rate_fast)
+        else -> stringResource(R.string.growth_rate_unknown)
     }
 }
 
 @Composable
 fun mapDeciduousEvergreenToString(deciduousEvergreen: String): String {
     return when (deciduousEvergreen) {
-        "D" -> stringResource(R.string.deciduous)       // Deciduous
-        "E" -> stringResource(R.string.evergreen)      // Evergreen
-        else -> stringResource(R.string.deciduous_evergreen_unknown) // Unknown Type
+        "D" -> stringResource(R.string.deciduous)
+        "E" -> stringResource(R.string.evergreen)
+        else -> stringResource(R.string.deciduous_evergreen_unknown)
     }
 }
 
 @Composable
 fun mapWellDrainedToString(isWellDrained: Boolean?): String {
     return when (isWellDrained) {
-        true -> stringResource(R.string.well_drained_yes)    // Well-drained
-        false -> stringResource(R.string.well_drained_no)   // Poorly-drained
-        null -> stringResource(R.string.well_drained_unknown) // Unknown
+        true -> stringResource(R.string.well_drained_yes)
+        false -> stringResource(R.string.well_drained_no)
+        null -> stringResource(R.string.well_drained_unknown)
     }
 }
 
 @Composable
 fun mapPoorSoilToString(toleratesPoorSoil: Boolean?): String {
     return when (toleratesPoorSoil) {
-        true -> stringResource(R.string.poor_soil_yes)    // Tolerates
-        false -> stringResource(R.string.poor_soil_no)   // Needs rich
-        null -> stringResource(R.string.poor_soil_unknown) // Unknown
+        true -> stringResource(R.string.poor_soil_yes)
+        false -> stringResource(R.string.poor_soil_no)
+        null -> stringResource(R.string.poor_soil_unknown)
     }
 }
 
 @Composable
 fun mapDroughtToString(isDroughtTolerant: Boolean?): String {
     return when (isDroughtTolerant) {
-        true -> stringResource(R.string.drought_yes)    // Tolerant
-        false -> stringResource(R.string.drought_no)   // Sensitive
-        null -> stringResource(R.string.drought_unknown) // Unknown
+        true -> stringResource(R.string.drought_yes)
+        false -> stringResource(R.string.drought_no)
+        null -> stringResource(R.string.drought_unknown)
     }
 }
