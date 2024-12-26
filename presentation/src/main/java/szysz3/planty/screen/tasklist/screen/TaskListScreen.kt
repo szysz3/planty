78ac72241efb4d4580fc3074fe2b0a92c799d6f0
Michal Szyszka <michalszyszka.m@gmail.com>
2024-12-26 17:29:14 +0100

    Revert "Adjust task card with more details"
    This reverts commit 8e0faa15dd944a45dd55a41a5fe2f162b10cdce7.

--- presentation/src/main/java/szysz3/planty/screen/tasklist/screen/TaskListScreen.kt
+++ presentation/src/main/java/szysz3/planty/screen/tasklist/screen/TaskListScreen.kt
@@ -59,10 +59,10 @@ fun TaskListScreen(
             draggableItems(items = tasks, dragDropState = dragDropState) { modifier, task ->
                 TaskCard(
                     task = task,
-                    onSubTaskCheckedChanged = { task, subtask, isChecked ->
-                        taskListViewModel.toggleSubTaskCompletion(task, subtask, isChecked)
-                    },
-                    onEditClicked = {}
+                    modifier = modifier,
+                    onTaskCheckedChanged = { task, isChecked ->
+                        taskListViewModel.toggleTaskCompletion(task, isChecked)
+                    }
                 )
             }
         }
