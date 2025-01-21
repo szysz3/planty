66611d14e18ee864d925d0513add547dc8b98057
Michal Szyszka <michalszyszka.m@gmail.com>
2025-01-21 17:38:33 +0100

    Add a bunch od kdocs for domain

--- domain/src/main/java/szysz3/planty/domain/usecase/base/BaseUseCase.kt
+++ domain/src/main/java/szysz3/planty/domain/usecase/base/BaseUseCase.kt
@@ -1,7 +1,23 @@
 package szysz3.planty.domain.usecase.base
 
+/**
+ * Base abstract class for use cases following clean architecture principles.
+ *
+ * @param Input The type of input parameters the use case expects
+ * @param Output The type of result the use case produces
+ */
 abstract class BaseUseCase<Input, Output> {
+    /**
+     * Executes the use case with the given input parameters.
+     *
+     * @param input The input parameters for the use case
+     * @return The result of the use case execution
+     */
     abstract suspend operator fun invoke(input: Input): Output
 }
 
+/**
+ * Utility class representing no parameters for use cases that don't require input.
+ * Used as a replacement for Unit to maintain consistency in use case signatures.
+ */
 class NoParams
